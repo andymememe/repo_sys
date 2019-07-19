d46304590cbe0341cc3b8c98d2bf1d20f1e8e0a1
andymememe <andymememe@gmail.com>
2019-07-20 00:18:25 +0800

    Feature: Add test for ConnectDB and GetRepos

--- repo_server/repo_serv_test.go
+++ repo_server/repo_serv_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+    "go.mongodb.org/mongo-driver/mongo/readpref";
     "testing";
     "os";
+    "context";
+    "regexp";
 )
 
 func TestLogMsg(t *testing.T) {
@@ -31,5 +34,52 @@ func TestCreateLog(t *testing.T) {
     
     if fi.Size() <= 0 {
         t.Errorf("Log file writing failed")
+    }
+}
+
+func TestConnectDB(t *testing.T) {
+    ctx := context.Background()
+    client, err := connectDB()
+    if err != nil {
+        t.Errorf("Connect DB error, %s", err.Error())
+    }
+    
+    err = client.Ping(ctx, readpref.Primary())
+    if err != nil {
+        t.Errorf("Ping error, %s", err.Error())
+    }
+}
+
+func TestGetRepos(t *testing.T) {
+    r, err := regexp.Compile("\\d{4}\\-\\d{2}\\-\\d{2}\\s+\\d{2}:\\d{2}:\\d{2}\\.\\d+\\s+[+-]*\\d{4}\\s+\\w+")
+    if err != nil {
+        panic(err.Error())
+    }
+
+    f, err := os.OpenFile("log/test_log.log",
+                          os.O_RDWR | os.O_CREATE | os.O_APPEND,
+                          0666)
+    if err != nil {
+        panic(err.Error())
+    }
+    logger := createLog(f)
+    
+    client, err := connectDB()
+    if err != nil {
+        panic(err.Error())
+    }
+    
+    collections, time, err := getRepos(logger, client)
+    if err != nil {
+        t.Errorf("Get repo error, %s", err.Error())
+    }
+    
+    timeStr := time.String()
+    if !r.MatchString(timeStr) {
+        t.Errorf("Time format error")
+    }
+    
+    if len(collections) <= 0 {
+        t.Errorf("Not getting any collection")
     }
 }
